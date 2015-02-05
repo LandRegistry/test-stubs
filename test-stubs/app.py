6b97606d385a9978da752734891cc656337c2d05
Andrew Moore <mooreandrewlr@gmail.com>
2015-02-05 12:06:23 +0000

    Improved Response Message

--- test-stubs/app.py
+++ test-stubs/app.py
@@ -9,9 +9,16 @@ tempstr = '';
 
 @app.route('/set_ppi', methods=['POST'])
 def set_tasks():
+
     global tempstr
-    tempstr = request.data
-    return request.data
+
+    ppi_amount = request.form['ppi_amount']
+    ppi_date = request.form['ppi_date']
+
+    message = '{ "head": { "vars": [ "amount" , "date" , "property_type" ] } , "results": { "bindings": [ { "amount": { "datatype": "http://www.w3.org/2001/XMLSchema#integer" , "type": "typed-literal" , "value": "' + ppi_amount + '" } , "date": { "datatype": "http://www.w3.org/2001/XMLSchema#date" , "type": "typed-literal" , "value": "' + ppi_date + '" } , "property_type": { "type": "uri" , "value": "http://landregistry.data.gov.uk/def/common/terraced" } } ] } }'
+
+    tempstr = message
+    return message
 
 @app.route('/get_ppi', methods=['POST'])
 def get_tasks():
